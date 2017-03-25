c11aa2b361aa077110ce347e81f126fbc06bbdc9
srinivas109 <thannerusrinivas59@gmail.com>
2017-03-25 21:18:53 +0530

    Add files via upload

---
+++ sim_prog/type1/Welcome1.java
@@ -0,0 +1,18 @@
+package srinivas.sim_prog.type1;
+
+// Fig. 2.3: Welcome2.java
+// Printing one line of text with multiple statements.
+
+public class Welcome1
+{
+   // main method begins execution of Java application
+   public static void main( String args[] )
+   {
+      System.out.print( "Welcome to " );
+      System.out.println( "Java Programming!" );
+      System.out.print( "Welcome again " );
+      
+   } // end method main
+
+} // end class Welcome2
+
---
+++ sim_prog/type2/Welcome2.java
@@ -0,0 +1,16 @@
+package srinivas.sim_prog.type2;
+
+// Fig. 2.4: Welcome2.java
+// Printing multiple lines with a single statement.
+
+public class Welcome2 
+{
+   // main method begins execution of Java application
+   public static void main( String args[] )
+   {
+      System.out.println( "Welcome\nto\nJava\nProgramming!" );
+
+   } // end method main
+
+} // end class Welcome2
+
